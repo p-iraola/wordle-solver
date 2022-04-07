5202f5fa37809e1096f3397b819eace6201ea50e
p-iraola <102985671+p-iraola@users.noreply.github.com>
2022-04-07 13:14:36 -0400

    Update wordle_solver.py

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,128 +1,99 @@
 from collections import Counter
-
-#Creating list of playable words
 flist = open('scrabble_words.txt').readlines()
 words = [word.rstrip('\n') for word in flist if len(word) == 6]
 
-guesses = 0
+incorrect_letters = []
+right_letter_wrong_place = []
+right_letter_right_place =[]
+guesses = 1
+comp_guess = ''
+
 while True:
-	
-	#Removing words that contain an incorrect letter
-	incorrect_letters = input('Enter any incorrect letters from your guess: ').strip()
+	if guesses > 7:
+		print('Even while cheating you still managed to fail.')
+
+	if comp_guess:
+		guess = comp_guess
+		guesses += 1
+	else:
+		guess = input('Enter your guess: ').strip()
+		while len(guess) != 5:
+			guess = input(str('Please enter a valid guess: ')).strip()
+		guesses += 1
+
+	print('\nEnter the result of the guess. '
+	'\nType 1 for a grey letter, 2 for a yellow letter, ' 
+	'or  3 for green letter.')
+
+	results = input('\nResults: ').strip()
+	while len(results) != 5 :
+		results = input(str('Please enter valid results: ')).strip()
+
+	#Match guess to results
+	for i, letter in enumerate(guess):
+		if results[i] == '1':
+			if letter not in incorrect_letters:
+				incorrect_letters.append(letter)
+		if results[i] == '2':
+			right_letter_wrong_place.append((i, letter))
+		if results[i] == '3':
+			right_letter_right_place.append((i, letter))
+
+	#Remove words with wrong letters
 	new_words = []
 	for word in words:
-		for incorrect_letter in incorrect_letters:
-			if incorrect_letter in word:
+		for incorrect in incorrect_letters:
+			if incorrect in word:
 				break
 		else:
 			new_words.append(word)
-
 	words = new_words
 
-	#Correct letter positioning
-	answer1 = input('\nAre there any letters in the correct position (y/n)? ').strip()
+	#Ensure all yellow letters are in the word
+	new_words = []
+	for word in words:
+		for i in range(len(right_letter_wrong_place)):
+			if right_letter_wrong_place[i][1] not in word:
+				break
+		else:
+			new_words.append(word)
+	words = new_words
 
-	while answer1 == 'y':
-		correct_words = []
-		position = int(input('\nWhich position (1,2,3,4,5)? '))
-		
-		if position == 1:
-			letter = input('\nEnter letter: ').strip()
-			for word in words:
-				if word[0] == letter:
-					correct_words.append(i)
+	#Remove words with incorrect yellow positioning
+	new_words = []
+	for word in words:
+		for position, letter in right_letter_wrong_place:
+			if word[position] == letter:
+				break
+		else:
+			new_words.append(word)
+	words = new_words
 
-		if position == 2:
-			letter = input('\nEnter letter: ').strip()
-			for word in words:
-				if word[1] == letter:
-					correct_words.append(i)
-
-		if position == 3:
-			letter = input('\nEnter letter: ').strip()
-			for word in words:
-				if word[2] == letter:
-					correct_words.append(i)
-
-		if position == 4:
-			letter = input('\nEnter letter: ').strip()
-			for word in words:
-				if word[3] == letter:
-					correct_words.append(i)
-
-		if position == 5:
-			letter = input('\nEnter letter: ').strip()
-			for word in words:
-				if word[4] == letter:
-					correct_words.append(i)
-
-		words = correct_words
-		answer1 = input('\nAny more (y/n)? ').strip()
-
-
-	#Incorrect letter positioning
-	answer2 = input('\nAre there any letters in an incorrect position (y/n)? ').strip()
-	if answer2 == 'y':
-		
-		must_haves = []
-		first = input('\nEnter first letter, or leave this blank: ').strip()
-		if first:
-			must_haves.append(first)
-
-		second = input('Enter second letter, or leave this blank: ').strip()
-		if second:
-			must_haves.append(second)
-
-		third = input('Enter third letter, or leave this blank: ').strip()
-		if third:
-			must_haves.append(third)
-
-		fourth = input('Enter fourth letter, or leave this blank: ').strip()
-		if fourth:
-			must_haves.append(fourth)
-
-		fifth = input('Enter fifth letter, or leave this blank: ').strip()
-		if fifth:
-			must_haves.append(fifth)
-
-		incorrect_position = []
+	#Remove words without correct green positioning
+	new_words = []
+	if right_letter_right_place:
 		for word in words:
-			if word[0] == first:
-				incorrect_position.append(word)
-			if word[1] == second:
-				incorrect_position.append(word)
-			if word[2] == third:
-				incorrect_position.append(word)
-			if word[3] == fourth:
-				incorrect_position.append(word)
-			if word[4] == fifth:
-				incorrect_position.append(word)
-		
-		new_words = []
-		for word in words:
-			for incorrect in incorrect_position:
-				if incorrect in word:
+			for position, letter in right_letter_right_place:
+				if word[position] != letter:
 					break
 			else:
 				new_words.append(word)
-
-		if new_words:
-			words = new_words
-
-		new_words = []
-		for word in words:
-			for must in must_haves:
-				if must not in word:
-					break
-			else:
-				new_words.append(word)
-		
 		words = new_words
-	print('\nPossible words: ' + str(words))
-
-	if len(words) == 1:
-		print('\nThe answer has to be: ' + str(words[0]).upper())
+	
+	if len(words) == 2:
+		print('\nLooks like you gotta guess between ' + str(words[0]) +
+		' and ' + str(words[1]) + '.')
 		break
+
+	if len(words) == 0:
+		print('There are no words that match your parameters.')
+		break
+	elif len(words) == 1:
+		print('\nThe correct word is ' + str(words[0].upper()) + '.')
+		print('It only took ' + str(guesses) + ' guesses.')
+		break
+	else:
+		print('Possible words: ' + str(words))
 	
 	#Guessing the next word
 	counts = Counter()
@@ -136,28 +107,20 @@ while True:
 		unique_chars = set(word)
 		word_score = sum([counts.get(c) for c in unique_chars])
 		word_scores.append((word_score, word))
-	
+
 	#Sorting the list and picking the word with the highest score     
 	comp_guess = sorted(word_scores, reverse=True)[0][1]
-	print('\nBest guess out of ' + str(len(words)) + ' words: ' + comp_guess.upper())
-	guesses += 1
-	if guesses == 5:
-		print("Even when you're cheating you're still a failure.")
-		break
+	print('\nBest guess out of ' + str(len(words)) + ' words: ' + 
+	comp_guess.upper())
 
 	#Reseting or ending loop
-	result = input('Was this right (y/n)? ').strip()
+	result = input('Was this right (y/n)? ').lower().strip()
+	while result != 'y' and result != 'n':
+		print('\nPlease enter y for yes or an n for no.')
+		result = input('Was this right (y/n)? ').lower().strip()
 	if result == 'y':
 		print('\n'+ comp_guess.upper() + ' was the correct word!')
-		print('And it only took ' + str(guesses) + ' guesses.')
+		print('It only took ' + str(guesses) + ' guesses.')
 		break
-	else:
-		words.remove(comp_guess)
-		if len(words) == 1:
-			guesses += 1
-			print('\nThen correct answer has to be: ' + str(words[0]).upper())
-			print('\nAnd it only took ' + str(guesses) + ' guesses.')
-			break
-		print("\nLet's try again.",
-		"\nThere's no need to re-enter any information from your previous guesses.")
-		print('\n')
+	elif result == 'n':
+		print("\nOk, then let's try again.")
